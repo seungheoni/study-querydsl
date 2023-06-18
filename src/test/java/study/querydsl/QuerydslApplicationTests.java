80a5a1c0235ab573cc0fc54b33451a0ac4fc218c
ued123 <32861341+ued123@users.noreply.github.com>
2023-06-19 07:34:57 +0900

    h2 환경 구성 및 jqa 테스트코드

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,7 +1,6 @@
 package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
